6b982132f91d3d04472d63e1a46eb24837658e32
bbhupen <bbhupen601@gmail.com>
2022-07-22 20:24:36 +0530

    add link to notification

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const path = require('path')
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 2000
 
 
 const app = express()
--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,12 @@
 self.addEventListener("push", (e) => {
 	const data = e.data.json();
 	console.log("Push Recieved...");
-	self.registration.showNotification(data.title, {
-		body: data.body,
-		icon: "https://res.cloudinary.com/freshlypick/image/upload/v1657122652/logo/iocl_omlheu.png",
-	});
+	self.registration.showNotification(data.title,
+		{
+			body: data.body,
+			icon: "https://res.cloudinary.com/freshlypick/image/upload/v1657122652/logo/iocl_omlheu.png",
+			data:{
+				url: JSON.parse(e.message).url
+			}
+		});
 });
